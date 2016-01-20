74d0930e6fed60143101c06d20d2d43d62d860c8
PJ Reiniger <pj.reiniger@gmail.com>
2016-01-20 18:17:00 -0500

    Adding averages to the all team display, adding an all match and single match display, adding menu bar

--- ScoutingWebsite2013/Scouting2013/views.py
+++ ScoutingWebsite2013/Scouting2013/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
-from Scouting2013.models import Team
+from django.db.models import Avg, Sum
+from Scouting2013.models import Team, Match
 
 # Create your views here.
+
+
+def __get_team_metrics(team):
+    metrics = team.scoreresult_set.aggregate(Avg('auton_score'), 
+                                          Avg('pyramid_goals'),
+                                          Avg('high_goals'),
+                                          Avg('mid_goals'),
+                                          Avg('low_goals'),
+                                          Avg('missed_shots'),
+                                          Avg('hanging_points'),
+                                          Avg('fouls'),
+                                          Avg('technical_fouls'),
+                                          Sum('invalid_hangs'),
+                                          Sum('yellow_card'),
+                                          Sum('red_card'),
+                                          Sum('broke_badly'),
+                                          )
+    
+    #Format all of the numbers.  If we haven't scouted the team, None will be returned.  Turn that into NA
+    for key in metrics:
+        if metrics[key] == None:
+            metrics[key] = "NA"
+        elif "__avg" in key:
+            metrics[key] = "{:10.2f}".format(metrics[key])
+            
+    return metrics
 
 
 def index(request):
@@ -11,9 +38,22 @@ def index(request):
 
 def all_teams(request):
 
-    all_team_types = Team.objects.all()
+    all_teams = Team.objects.all()
+    
+    teams_with_avg = []
+    
+    for team in all_teams:
+        
+        metrics = __get_team_metrics(team)
+                
+        team_with_avg = {"id": team.id, 
+                         "teamNumber": team.teamNumber,
+                         "matches_scouted": team.scoreresult_set.count(),
+                         "avgerages": metrics,
+                        }
+        teams_with_avg.append(team_with_avg)
 
-    context = {"teams": all_team_types}
+    context = {"teams": teams_with_avg}
 
     return render(request, 'Scouting2013/all_teams.html', context)
 
@@ -21,17 +61,40 @@ def all_teams(request):
 def view_team(request, team_id):
 
     this_team = Team.objects.get(id=team_id)
+    
+    metrics = __get_team_metrics(this_team)
+    match_list = []
+    
+    for sr in this_team.scoreresult_set.all():
+        match_list.append(sr.match)
+    
 
-    context = {"team": this_team}
+    context = {"id": this_team.id, 
+               "teamNumber": this_team.teamNumber,
+               "metrics": metrics,
+               "match_list": match_list,
+              }
 
     return render(request, 'Scouting2013/single_team.html', context)
 
 
 def all_matches(request):
+    
+    all_matches = Match.objects.all()
+    
+    context = {"matches": all_matches}
 
-    return render(request, 'Scouting2013/all_matches.html')
+    return render(request, 'Scouting2013/all_matches.html', context)
 
 
-def view_match(request):
+def view_match(request, match_id):
 
-    return render(request, 'Scouting2013/single_match.html')
+    this_match = Match.objects.get(id=match_id)
+    results = this_match.scoreresult_set.all()
+    
+    context = {"id": this_match.id, 
+               "matchNumber": this_match.matchNumber,
+               "results": results,
+              }
+
+    return render(request, 'Scouting2013/single_match.html', context)
