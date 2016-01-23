ef31f22409610f109f0aa12fb2122da4f4ce46c8
Ayush Goel <goelayush@mylcsd.org>
2016-01-23 13:22:16 -0500

    Scouting Form build

--- ScoutingWebsite2015/Scouting2016/urls.py
+++ ScoutingWebsite2015/Scouting2016/urls.py
@@ -7,6 +7,9 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = 'Scouting2016'
 urlpatterns = [
                url(r'^$', views.index, name='index'),
+               url(r'^form$', views.showForm, name='showForm'),
+               url(r'^submit_form$', views.submitForm, name='submit_form'),
               ]
--- ScoutingWebsite2015/Scouting2016/views.py
+++ ScoutingWebsite2015/Scouting2016/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.template.context_processors import request
 
 # Create your views here.
 
 def index(request):
 
+    return render(request, 'Scouting2016/index.html')
+
+def showForm(request):
+
+    return render(request, 'Scouting2016/inputForm.html')
+
+def submitForm(request):
+    print "Submitting Form"
+    print request.POST
     return render(request, 'Scouting2016/index.html')
