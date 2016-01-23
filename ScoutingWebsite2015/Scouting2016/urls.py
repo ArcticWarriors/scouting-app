0fb8b05c49b69eb49d0ba35e62429779fb9802cc
wilsonaar <wilsonaar@mylcsd.org>
2016-01-23 15:27:49 -0800

    Adding team & match pages , editing style.css, adding image,

--- ScoutingWebsite2015/Scouting2016/urls.py
+++ ScoutingWebsite2015/Scouting2016/urls.py
@@ -10,4 +10,8 @@ from . import views
 urlpatterns = [
                url(r'^$', views.index, name='index'),
                 url(r'^robot_display$', views.robot_display, name='robot_display'),
+                url (r'^view_team/(?P<team_number>[0-9]+)$', views.view_team, name = 'view_team'),
+                url (r'^match_display/(?P<match_number>[0-9]+)$', views.match_display, name = 'match_display'),
+                 url(r'^all_teams$', views.all_teams, name='all_teams'),
+                 url(r'^all_matches$', views.all_matches, name='all_matches'),
               ]
--- ScoutingWebsite2015/Scouting2016/views.py
+++ ScoutingWebsite2015/Scouting2016/views.py
@@ -8,3 +8,20 @@ def index(request):
 
 def robot_display(request):
     return render(request, 'Scouting2016/RobotDisplay.html')
+
+def view_team(request, team_number):
+    context = {}
+    context['team_number'] = team_number
+    return render(request, 'Scouting2016/TeamPage.html', context)
+
+def match_display(request, match_number):
+    context = {}
+    context['match_display'] = match_number
+    print context
+    return render(request, 'Scouting2016/MatchPage.html', context)
+
+def all_teams(request):
+    return render(request, 'Scouting2016/AllTeams.html')
+
+def all_matches(request):
+    return render(request, 'Scouting2016/AllMatches.html')
