d0bce7bf7a4e1de6470051ff2d1dd54ca2fd05d8
PJ Reiniger <pj.reiniger@gmail.com>
2016-01-31 20:57:24 -0500

    Ran Formatting

--- ScoutingWebsite2013/Scouting2013/urls.py
+++ ScoutingWebsite2013/Scouting2013/urls.py
@@ -7,12 +7,11 @@ from django.conf.urls import url
 from . import views
 
 
-urlpatterns = [
-               url(r'^$', views.index, name='index'),
+urlpatterns = [url(r'^$', views.index, name='index'),
                url(r'^teams$', views.all_teams, name='all_teams'),
                url(r'^teams/(?P<team_id>[0-9]+)$', views.view_team, name='view_team'),
                url(r'^match$', views.all_matches, name='all_matches'),
                url(r'^match/(?P<match_id>[0-9]+)$', views.view_match, name='view_match'),
                url(r'^view_graph$', views.view_graph, name='view_graph'),
                url(r'^create_metrics_plot/teams=(?P<team_ids>\w+(,\w+)*),fields=(?P<fields>\w+(,\w+)*)', views.create_metrics_plot, name='create_metrics_plot'),
-              ]
+               ]
--- ScoutingWebsite2013/Scouting2013/views.py
+++ ScoutingWebsite2013/Scouting2013/views.py
@@ -7,7 +7,7 @@ from Scouting2013.models import Team, Match
 
 
 def __get_team_metrics(team):
-    metrics = team.scoreresult_set.aggregate(Avg('auton_score'      ), 
+    metrics = team.scoreresult_set.aggregate(Avg('auton_score'),
                                              Avg('pyramid_goals'    ),
                                              Avg('high_goals'       ),
                                              Avg('mid_goals'        ),
@@ -21,14 +21,14 @@ def __get_team_metrics(team):
                                              Sum('red_card'         ),
                                              Sum('broke_badly'      ),
                                           )
-    
-    #Format all of the numbers.  If we haven't scouted the team, None will be returned.  Turn that into NA
+
+    # Format all of the numbers.  If we haven't scouted the team, None will be returned.  Turn that into NA
     for key in metrics:
         if metrics[key] == None:
             metrics[key] = "NA"
         elif "__avg" in key:
             metrics[key] = "{:10.2f}".format(metrics[key])
-            
+
     return metrics
 
 
@@ -40,18 +40,18 @@ def index(request):
 def all_teams(request):
 
     all_teams = Team.objects.all()
-    
+
     teams_with_avg = []
-    
+
     for team in all_teams:
-        
+
         metrics = __get_team_metrics(team)
-                
-        team_with_avg = {"id": team.id, 
+
+        team_with_avg = {"id": team.id,
                          "teamNumber": team.teamNumber,
                          "matches_scouted": team.scoreresult_set.count(),
                          "avgerages": metrics,
-                        }
+                         }
         teams_with_avg.append(team_with_avg)
 
     context = {"teams": teams_with_avg}
@@ -62,15 +62,14 @@ def all_teams(request):
 def view_team(request, team_id):
 
     this_team = Team.objects.get(id=team_id)
-    
+
     metrics = __get_team_metrics(this_team)
     match_list = []
-    
+
     for sr in this_team.scoreresult_set.all():
         match_list.append(sr.match)
-    
 
-    context = {"id": this_team.id, 
+    context = {"id": this_team.id,
                "teamNumber": this_team.teamNumber,
                "metrics": metrics,
                "match_list": match_list,
@@ -80,9 +79,9 @@ def view_team(request, team_id):
 
 
 def all_matches(request):
-    
+
     all_matches = Match.objects.all()
-    
+
     context = {"matches": all_matches}
 
     return render(request, 'Scouting2013/all_matches.html', context)
@@ -92,37 +91,37 @@ def view_match(request, match_id):
 
     this_match = Match.objects.get(id=match_id)
     results = this_match.scoreresult_set.all()
-    
-    context = {"id": this_match.id, 
+
+    context = {"id": this_match.id,
                "matchNumber": this_match.matchNumber,
                "results": results,
-              }
+               }
 
     return render(request, 'Scouting2013/single_match.html', context)
 
 
 def view_graph(request, team_ids=[], fields=[]):
-    
+
     try:
         import matplotlib
         team_ids = [3, 4]
         fields = "auton_score,high_goals"
-        
+
         team_numbers = []
         for team_id in team_ids:
             team = Team.objects.get(id=team_id)
             team_numbers.append(team.teamNumber)
-        
+
         context = {"team_ids": ",".join(str(x) for x in team_ids),
                    "team_numbers": team_numbers,
                    "field_list": fields,
-                  }
-        
+                   }
+
         return render(request, 'Scouting2013/view_graph.html', context)
     except:
         pass
-    
-    return HttpResponseNotFound('<h1>Could not import matplot lib, cannot plot</h1>') 
+
+    return HttpResponseNotFound('<h1>Could not import matplot lib, cannot plot</h1>')
 
 
 def create_metrics_plot(request, team_ids, fields):
@@ -132,16 +131,16 @@ def create_metrics_plot(request, team_ids, fields):
     import matplotlib.pyplot as plt
     from matplotlib.backends.backend_agg import FigureCanvasAgg
     from matplotlib.font_manager import FontProperties
-    
+
     team_ids = [str(x) for x in team_ids.split(",")]
-    fields = [str(x) for x in fields.split(",")]    
-    
-    f = plt.figure(figsize=(6,6))
+    fields = [str(x) for x in fields.split(",")]
+
+    f = plt.figure(figsize=(6, 6))
     legend_handles = []
-    
+
     for team_id in team_ids:
         team = Team.objects.get(id=team_id)
-        
+
         for field in fields:
             metric = []
             for result in team.scoreresult_set.all():
@@ -149,16 +148,16 @@ def create_metrics_plot(request, team_ids, fields):
             print field
             hand, = plt.plot(metric, label="Team %s, %s" % (team.teamNumber, field))
             legend_handles.append(hand)
-            
+
     fontP = FontProperties()
     fontP.set_size('small')
     plt.legend(handles=legend_handles, prop=fontP)
     plt.xlabel("Match")
-    
+
     matplotlib.pyplot.close(f)
-    
-    canvas = FigureCanvasAgg(f)    
+
+    canvas = FigureCanvasAgg(f)
     response = HttpResponse(content_type='image/png')
     canvas.print_png(response)
-    
+
     return response
