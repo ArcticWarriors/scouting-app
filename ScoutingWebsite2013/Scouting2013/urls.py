39bf8d2db640659ded8b358b41bfb6f9c95b96db
PJ Reiniger <pj.reiniger@gmail.com>
2016-01-31 21:47:51 -0500

    Updating 2013 scouting to have the same pages as 2016, to illistrate any potential differences in making this tool more reusable

--- ScoutingWebsite2013/Scouting2013/urls.py
+++ ScoutingWebsite2013/Scouting2013/urls.py
@@ -8,10 +8,15 @@ from . import views
 
 
 urlpatterns = [url(r'^$', views.index, name='index'),
-               url(r'^teams$', views.all_teams, name='all_teams'),
-               url(r'^teams/(?P<team_id>[0-9]+)$', views.view_team, name='view_team'),
-               url(r'^match$', views.all_matches, name='all_matches'),
-               url(r'^match/(?P<match_id>[0-9]+)$', views.view_match, name='view_match'),
-               url(r'^view_graph$', views.view_graph, name='view_graph'),
-               url(r'^create_metrics_plot/teams=(?P<team_ids>\w+(,\w+)*),fields=(?P<fields>\w+(,\w+)*)', views.create_metrics_plot, name='create_metrics_plot'),
-               ]
+
+               # Normal Pages
+               url(r'^robot_display$', views.robot_display, name='robot_display'),
+               url(r'^view_team/(?P<team_number>[0-9]+)$', views.view_team, name='view_team'),
+               url(r'^match_display/(?P<match_number>[0-9]+)$', views.match_display, name='match_display'),
+               url(r'^all_teams$', views.all_teams, name='all_teams'),
+               url(r'^all_matches$', views.all_matches, name='all_matches'),
+               url(r'^search$', views.search_page, name='search_page'),
+               url(r'^graph$', views.show_graph, name='show_graph'),
+               url(r'^gen_graph/(?P<team_numbers>\w+(,\w+)*)/(?P<fields>\w+(,\w+)*)$', views.gen_graph, name='gen_graph'),
+               url(r'^match_prediction/(?P<match_number>[0-9]+)$', views.match_prediction, name='match_prediction'),
+              ]
