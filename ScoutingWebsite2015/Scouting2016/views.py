73cb00a90c9565c5bbf1d8f3be1408bf99cc62de
wilsonaar <wilsonaar@mylcsd.org>
2016-02-19 20:09:04 -0500

    Added tons of into to match page.
    Did that
    Also centered text in team page tables

--- ScoutingWebsite2015/Scouting2016/views.py
+++ ScoutingWebsite2015/Scouting2016/views.py
@@ -355,7 +355,7 @@ def get_defense_names():
     defenses.append('moat')
     defenses.append('rock_wall')
     defenses.append('rough_terrain')
-
+#   defenses.append('dragon_fire')
     return defenses
 
 
