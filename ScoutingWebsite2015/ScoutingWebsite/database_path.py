1406e2998724d09162bc87521400bda7ea47c340
mrcro503 <mrcro503@gmail.com>
2016-03-23 15:30:56 -0400

    Switched to live DB

--- ScoutingWebsite2015/ScoutingWebsite/database_path.py
+++ ScoutingWebsite2015/ScoutingWebsite/database_path.py
@@ -1,2 +1,2 @@
-# database_path = './NYRO.sqlite3'
-database_path = './__api_scraping_results/database/week3/NYTR.sqlite3'
+database_path = './NYRO.sqlite3'
+# database_path = './__api_scraping_results/database/week3/NYTR.sqlite3'
