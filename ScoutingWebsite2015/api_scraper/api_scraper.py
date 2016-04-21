d2e6734127bb5a5b450301e6a23edbe8996f24ad
PJ Reiniger <pj.reiniger@gmail.com>
2016-04-20 23:00:41 -0400

    Missing files

--- ScoutingWebsite2015/api_scraper/api_scraper.py
+++ ScoutingWebsite2015/api_scraper/api_scraper.py
@@ -9,10 +9,12 @@ import json
 from ApiDownloader import ApiDownloader
 
 
-year = 2015
+year = 2016
 json_root = os.path.abspath("../Scouting{0}/__api_scraping_results".format(year)) + "/"
 scraper = ApiDownloader(year, json_root)
 
+# scraper.download_event_data()
+# scraper.download_team_data()
 
 events_file = json_root + 'events/event_week_mapping.json'
 
@@ -23,5 +25,3 @@ for pair in json_struct:
     scraper.download_schedule(pair[0], pair[1])
     scraper.download_matchresult_info(pair[0], pair[1])
 
-# scraper.download_event_data()
-# scraper.download_team_data()
