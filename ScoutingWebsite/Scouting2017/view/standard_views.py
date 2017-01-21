f7307fbff1fb0adf6fd052b8c0ae30107a47b7e5
wilsonaar <wilsonaar@mylcsd.org>
2017-01-21 15:52:11 -0500

    started finding st. devs. Push because it is 4pm

--- ScoutingWebsite/Scouting2017/view/standard_views.py
+++ ScoutingWebsite/Scouting2017/view/standard_views.py
@@ -1,7 +1,7 @@
 from BaseScouting.views.base_views import BaseHomepageView, BaseAllTeamsViews,\
     BaseAllMatchesView, BaseSingleTeamView
 from Scouting2017.model.reusable_models import Competition, TeamCompetesIn, Match, OfficialMatch, Team, TeamPictures, TeamComments
-from Scouting2017.model.models2017 import get_team_metrics
+from Scouting2017.model.models2017 import get_team_metrics, ScoreResult
 
 class HomepageView2017(BaseHomepageView):
  
@@ -24,6 +24,36 @@ class AllTeamsViews2017(BaseAllTeamsViews):
 
     def get_metrics_for_team(self, team):
         return get_team_metrics(team)
+    
+    def get_context_data(self, **kwargs):
+        context = BaseAllTeamsViews.get_context_data(self, **kwargs)
+        context['statistics'] = self.get_statistics(kwargs['regional_code'])
+        
+    
+    def get_statistics(self, regional_code):
+
+        competition_srs = ScoreResult.objects.filter(competition__code=regional_code)
+        teams_at_competition = TeamCompetesIn.objects.filter(competition__code=regional_code)
+        metrics = get_team_metrics(teams_at_competition)
+        # Average All the stuff
+        global_gear_sum = 0
+        teams_with_gears = 0
+        
+        for team in metrics:
+            global_gear_sum .join(team.gears_score)
+            teams_with_gears += 1
+    
+       
+        
+        global_gear_avg = 0 #some number
+        
+        for team in teams_at_competition:
+            pass
+        
+            #compare global to me
+            
+        
+            
     
 class AllMatchesViews2017(BaseAllMatchesView):
     def __init__(self):
