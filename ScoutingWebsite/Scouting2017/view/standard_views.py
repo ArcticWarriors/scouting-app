afee5dcd93afabcbf28768ecea53dbfca9d7a427
wilsonaar <wilsonaar@mylcsd.org>
2017-02-03 19:35:11 -0500

    LOTS OF STATISTAROONIES

--- ScoutingWebsite/Scouting2017/view/standard_views.py
+++ ScoutingWebsite/Scouting2017/view/standard_views.py
@@ -31,10 +31,9 @@ class TeamListView2017(BaseTeamListView):
     def get_context_data(self, **kwargs):
         context = BaseTeamListView.get_context_data(self, **kwargs)
         reg_code = kwargs['regional_code']
-        
 #         teams_at_competition = TeamCompetesIn.objects.filter(competition__code=reg_code)
-        
-        get_statistics(reg_code, context['teams']) 
+        stats = get_statistics(reg_code, context['teams'])
+        context['stats'] = stats 
         return context
         
 class SingleMatchView2017(BaseSingleMatchView):
@@ -76,14 +75,13 @@ def get_statistics(regional_code, teams_at_competition):
                                                     rope__avg = Avg(Case(When(rope=True, then=1),When(rope=False, then=0))))
     num_srs = 0 
     rope_avg = competition_averages['rope__avg']
-    print rope_avg
     gear_avg = competition_averages['gears_score__avg']
     fuel_avg = competition_averages['fuel_score_hi_auto__avg'] + (competition_averages['fuel_score_hi__avg'] / 3 ) + (competition_averages['fuel_score_low_auto__avg'] / 3) + (competition_averages['fuel_score_low__avg'] / 9)
     gear_v2 = 0
     fuel_v2 = 0
     rope_v2 = 0
     num_srs = 0 
-
+    print gear_avg
     
     for sr in competition_srs: 
         if sr.rope==True:
@@ -99,7 +97,6 @@ def get_statistics(regional_code, teams_at_competition):
     gear_stdev = math.sqrt(gear_v2/num_srs) 
     fuel_stdev = math.sqrt(fuel_v2/num_srs)
     rope_stdev = math.sqrt(rope_v2/num_srs)
-    print rope_stdev
     
     for team in teams_at_competition:
         teams_srs = team.scoreresult_set.filter(competition__code=regional_code) 
@@ -111,13 +108,15 @@ def get_statistics(regional_code, teams_at_competition):
                                         team_rope__avg = Avg(Case(When(rope=True, then=1),When(rope=False, then=0))))
                                       
         team_rope_avg = team_avgs['team_rope__avg']
-        print team_rope_avg
         team.fuel_z = 'NA'
         team.gear_z = 'NA'
         team.rope_z = 'NA'
         team.rope_pct = 'NA'
         if len(teams_srs)!= 0:
+            team.fuel_score = ((team_avgs['fuel_score_hi_auto__avg']) + (team_avgs['fuel_score_hi__avg'] / 3) + (team_avgs['fuel_score_low_auto__avg'] / 3) + (team_avgs['fuel_score_low__avg']/ 9))
             team.gear_z = (team_avgs['gears_score__avg'] - gear_avg ) / gear_stdev 
             team.fuel_z = (((team_avgs['fuel_score_hi_auto__avg']) + (team_avgs['fuel_score_hi__avg'] / 3) + (team_avgs['fuel_score_low_auto__avg'] / 3) + (team_avgs['fuel_score_low__avg']/ 9)) - fuel_avg) / fuel_stdev
             team.rope_z = (team_avgs['team_rope__avg'] - rope_avg) / rope_stdev
             team.rope_pct = team_avgs['team_rope__avg'] * 100
+    stats = {'gear_avg': gear_avg, 'rope_avg': rope_avg, 'fuel_avg': fuel_avg, 'fuel_hi_avg': team_avgs['fuel_score_hi__avg'], 'fuel_low_avg': team_avgs['fuel_score_low__avg'], 'gear_stdev': gear_stdev, 'rope_stdev': rope_stdev, 'fuel_stdev': fuel_stdev}
+    return stats   
